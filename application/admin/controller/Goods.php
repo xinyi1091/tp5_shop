b51527bf6e46bc88792027b9d165c7da62339543
nanzaichen <zuixuanminzufeng@163.com>
2020-12-15 13:19:12 +0800

    继承百度编辑器、添加商品、表单验证

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 use think\Controller;
 use app\admin\model\Goods as GoodsModel;
+use think\Request;
+use think\Validate;
 
 class Goods extends Controller
 {
@@ -15,8 +17,45 @@ class Goods extends Controller
         return view('index', ['list' => $list]);
     }
 
-    public function create()
+    public function create(Request $request)
     {
+        // 使用依赖注入的$request对象
+        $data = $request->param();
+        if ($data) {
+            // 1. 定义验证规则
+            $rules = [
+                'goods_name'   => 'require',
+                'goods_price'  => 'require|float|gt:0',
+                'goods_number' => 'require|integer|gt:0',
+            ];
+            // 2. 定义错误提示
+            $msg    = [
+                'goods_name.require'   => '商品名称不能为空',
+                'goods_price.require'  => '商品价格不能为空',
+                'goods_price.float'    => '商品价格格式不正确',
+                'goods_price.gt'       => '商品价格必须大于0',
+                'goods_number.require' => '商品数量不能为空',
+                'goods_number.float'   => '商品数量格式不正确',
+                'goods_number.gt'      => '商品数量必须大于0',
+            ];
+            // 3. 实例化验证器类 \think\Validate()
+            $validate = new Validate($rules, $msg);
+            // 4. 进行验证
+            if (!$validate->check($data)) {
+                // 验证失败，调用getError方法获取具体的错误提示
+                $error = $validate->getError();
+                $this->error($error);
+            }
+            // 使用静态create方法保存数据，第二个参数表示过滤非数据表中的字段
+            $res = GoodsModel::create($data, true);
+            if ($res) {
+                $this->success('添加成功', 'index');
+            } else {
+                //错误页面的默认跳转页面是返回前一页，通常不需要设置
+                $this->error('新增失败');
+            }
+
+        }
         return view();
     }
 
